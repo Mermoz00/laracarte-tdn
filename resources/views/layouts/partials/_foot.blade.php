019244f60b22348462ce76d4307755df5b5f7958
Nestor Mermoz Thea <nestormermozthea@gmail.com>
2019-09-02 17:44:31 +0200

    Setup and add static pages

---
+++ resources/views/layouts/partials/_foot.blade.php
@@ -0,0 +1,8 @@
+<footer class="text-center">
+  <p>
+    &copy; 2019 &middot;Laracarte by<a href="#">@etsmo</a> - A <a href="https://laramap.com" target="_blank">Laramap</a> clone app.
+  </p>
+  <p class="text-center">
+    <b>This app has been built for learning purposes.</b>
+  </p>
+  </footer>
